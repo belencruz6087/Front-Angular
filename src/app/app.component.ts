097357099045594370a43fa6f9e7ef5a8bca2b15
belencruz6087 <belucruz@gmail.com>
2022-07-28 22:15:38 -0300

    Agrego cambio portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,43 @@
 import { Component } from '@angular/core';
-
+import  { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+ 
+
+ export class AppComponent {
+  constructor() {};
   title = 'Portfolio Belen Cruz';
+  ngOnInit(): void {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: [            
+            ],
+            datasets: [{
+                data: [55, 100],
+                borderColor: 'gray',
+                backgroundColor: [
+                    'green',
+                    'white',
+                    ],
+                hoverOffset: 4,
+                hoverBorderColor: "black",
+                
+                
+            }]
+        },
+        _options: { responsive: true }
+    });
+   }
   
 }
+
 
 
 
